2f070ecbacda507fdac8bf0b478583fd11edaf0a
Bwallker <benbwall@gmail.com>
2023-11-15 21:44:53 +0200

    Add util file

---
+++ src/util.rs
@@ -0,0 +1,11 @@
+use serenity::prelude::{TypeMap, TypeMapKey};
+use tokio::sync::RwLock;
+
+pub async fn get_value<T>(map: &RwLock<TypeMap>) -> T::Value
+where
+    T: TypeMapKey,
+    T::Value: Clone,
+{
+    let lock = map.read().await;
+    lock.get::<T>().unwrap().clone()
+}
