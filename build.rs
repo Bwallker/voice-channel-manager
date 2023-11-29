2d65fafcc30daffd5a751282d35d9ce03ebf9869
bwallker <benbwall@gmail.com>
2023-11-29 11:24:07 +0200

    Fix bug in build.rs

--- build.rs
+++ build.rs
@@ -6,16 +6,16 @@ fn main() {
     // trigger recompilation when a new migration is added
     println!("cargo:rerun-if-changed=migrations");
 
-    let rust_toolchain = var("RUSTUP_TOOLCHAIN",).unwrap();
-
-    if rust_toolchain.starts_with("stable",) {
-        // do nothing
-    } else if rust_toolchain.starts_with("nightly",) {
-        // enable the 'nightly-features' feature flag
-        println!("cargo:rustc-cfg=feature=\"nightly-features\"");
-    } else if rust_toolchain.starts_with("beta",) {
-        println!("cargo:rustc-cfg=feature=\"beta-features\"");
-    } else {
-        panic!("Unexpected value for rustc toolchain")
+    if let Ok(rust_toolchain,) = var("RUSTUP_TOOLCHAIN",) {
+        if rust_toolchain.starts_with("stable",) {
+            // do nothing
+        } else if rust_toolchain.starts_with("nightly",) {
+            // enable the 'nightly-features' feature flag
+            println!("cargo:rustc-cfg=feature=\"nightly-features\"");
+        } else if rust_toolchain.starts_with("beta",) {
+            println!("cargo:rustc-cfg=feature=\"beta-features\"");
+        } else {
+            panic!("Unexpected value for rustc toolchain")
+        }
     }
 }
